706734a75d5c58b22c965f64a54a0e4393a0e0ce
aimee <amandapraco@gmail.com>
2025-11-05 11:32:15 -0300

    Removido bloco <details> e referências relacionadas

---
+++ HTML/Form/script.js
@@ -0,0 +1,61 @@
+/*
+let num = 5;
+console.log(typeof num);
+num = prompt("Digite um número");
+console.log(typeof num);
+num = num + " " + num;
+console.log(num);
+
+num = parseInt(num)
+//num = num>10;
+console.log(num);
+if (num>10) {
+    alert("O valor é maior que dez");
+}
+
+else if (num == 10) {
+    alert("O valor é dez")
+
+}
+
+else {
+    alert("O valor é menor que dez");
+}
+*/
+
+//add.EventListener ("input") execute a função, quando o usuário digitar no input//
+//DOMContentLoaded = espere o DOM carregar//
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const campoNome = document.getElementById("campo_nome");
+//     const campoEmail = document.getElementById("campo_email");
+//     const campoSenha = document.getElementById("campo_senha");
+//     const campoSenha2 = document.getElementById("campo_senha2");
+  
+//     const saidaNome = document.getElementById("saida_nome_valor");
+//     const saidaEmail = document.getElementById("saida_email_valor");
+//     const saidaSenha = document.getElementById("saida_senha_valor");
+//     const saidaSenha2 = document.getElementById("saida_senha2_valor");
+  
+//     campoNome.addEventListener("input", () => {
+//       console.log(campoNome);
+//       saidaNome.textContent = campoNome.value;
+//     });
+  
+//     campoEmail.addEventListener("input", () => {
+//       saidaEmail.textContent = campoEmail.value;
+//     });
+  
+//     campoSenha.addEventListener("input", () => {
+//       saidaSenha.textContent = campoSenha.value;
+//     });
+  
+//     campoSenha2.addEventListener("input", () => {
+//       saidaSenha2.textContent = campoSenha2.value;
+//     });
+//   });
+
+//trim tira os espaços em branco do inicio e do fim da string//
+
+
+  
